45da5cca6818430fbd919ad7541d5f2ea15d781b
nikolaygubin <nikolaygubin@example.com>
2024-02-12 19:19:56 +0700

    salam

--- telegram_bot.py
+++ telegram_bot.py
@@ -13,32 +13,67 @@ admin.register_handlers_admin(dp)
 client.register_handlers_client(dp)
 
 logging.basicConfig(level=logging.INFO)
-    
+
+
 async def on_startup(dp):
     sqlite_db.start_sql()
-    scheduler.add_job(del_promo, 'cron', hour='9', minute='0', timezone='Europe/Moscow')
-    scheduler.add_job(make_pairs, 'cron', day_of_week='0', hour='10', minute='0', timezone='Europe/Moscow')
-    scheduler.add_job(ask_impress, 'cron', day_of_week='3,5', hour='12', minute='0', timezone='Europe/Moscow')
-    scheduler.add_job(is_active, 'cron', day_of_week='6', hour='10', minute='0', timezone='Europe/Moscow')
-    scheduler.add_job(update_paid, 'cron', day_of_week='6', hour='8', minute='0', timezone='Europe/Moscow')
-    scheduler.add_job(send_invoices_task, 'cron', day_of_week='0', hour='10', minute='30', timezone='Europe/Moscow')
+    scheduler.add_job(del_promo, "cron", hour="9", minute="0", timezone="Europe/Moscow")
+    scheduler.add_job(
+        make_pairs,
+        "cron",
+        day_of_week="0",
+        hour="10",
+        minute="0",
+        timezone="Europe/Moscow",
+    )
+    scheduler.add_job(
+        ask_impress,
+        "cron",
+        day_of_week="3,5",
+        hour="12",
+        minute="0",
+        timezone="Europe/Moscow",
+    )
+    scheduler.add_job(
+        is_active,
+        "cron",
+        day_of_week="6",
+        hour="10",
+        minute="0",
+        timezone="Europe/Moscow",
+    )
+    scheduler.add_job(
+        update_paid,
+        "cron",
+        day_of_week="6",
+        hour="8",
+        minute="0",
+        timezone="Europe/Moscow",
+    )
+    scheduler.add_job(
+        send_invoices_task,
+        "cron",
+        day_of_week="0",
+        hour="10",
+        minute="30",
+        timezone="Europe/Moscow",
+    )
     scheduler.start()
-    await bot.set_webhook(os.environ.get('URL'))
+    await bot.set_webhook(os.environ.get("URL"))
+
 
 async def on_shutdown(dp):
     await sqlite_db.close_db()
     await bot.delete_webhook()
 
-    
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     executor.start_webhook(
-        dispatcher = dp,
-        webhook_path = '',
-        skip_updates = True,
-        on_startup = on_startup,
-        on_shutdown = on_shutdown,
-        host = "0.0.0.0",
-        port = int(os.environ.get("PORT", 5000))
+        dispatcher=dp,
+        webhook_path="",
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
     )
-
